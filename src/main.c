4b4c99af49b7933465557ed78f6989a63f803585
Valiance <lucas.ludovic@epitech.eu>
2023-12-22 14:14:21 +0100

    [UPDATE] Check the environment

--- src/main.c
+++ src/main.c
@@ -30,6 +30,16 @@ int display_help(void)
 static
 int check_env(UNUSED char **env)
 {
+    int env_found = 0;
+
+    for (int i = 0; env[i] != NULL; i += 1) {
+        if (my_strncmp(env[i], "DISPLAY=", 8) == 0)
+            env_found += 1;
+        if (my_strncmp(env[i], "XAUTHORITY=", 11) == 0)
+            env_found += 1;
+    }
+    if (env_found != 2)
+        return display_error("Can't open a graphical program\n");
     return SUCCESS;
 }
 
